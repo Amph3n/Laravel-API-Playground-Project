ac747e6395c673f9021251163df789a0c9f05458
Amph3n <oliver_sommer@live.dk>
2023-08-05 12:12:06 +0200

    Start on Todo List
    Controller, model and migration created.
    Also some tailwind on the index page of the site. Migration haven't been run yet.

---
+++ resources/js/Pages/Todo/Index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+import Modal from '@/Components/Modal.vue';
+import { ref } from 'vue';
+
+const modalOpen = ref(false);
+</script>
+
+<template>
+
+<Head title="Todos"  />
+
+<AuthenticatedLayout>
+    <div class="flex justify-end max-w-2xl p-4 mx-auto sm:p-6 lg:p-8">
+        <PrimaryButton @click="modalOpen = true">New Todo</PrimaryButton>
+    </div>
+
+    <Modal :show="modalOpen">
+        <div class="flex justify-end w-full h-9">
+            <button @click="modalOpen = false" class="w-8 h-full rounded bg-cyan-700"></button>
+        </div>
+        <p>Hello modal :)</p>
+    </Modal>
+
+</AuthenticatedLayout>
+
+</template>
+
+<style>
+
+</style>
